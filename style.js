c8e492e88e1c268a265c547a04059ac8d70475ad
Irina <il064330@gmail.com>
2023-07-16 19:59:58 +0300

    corrected errors in js file

--- style.js
+++ style.js
@@ -4,23 +4,16 @@ for (let i = 0; i < price.length; i++) {
   sumPrice += +(price[i].innerHTML);
 }
 
-//console.log(sumPrice)
 let cartSum = document.querySelector("#cart_sum");
 cartSum.innerHTML = sumPrice;
 
-//price.forEach(function(element) {
-  //console.log(element.textContent);
-  //});
-
 document.getElementById("button").onclick = function () {
-  //price.forEach(function(element) {
-  //+(element.textContent) * 0.8
-  //console.log(price)
-   // });
-  
+   for (let i = 0; i < price.length; i++) {
+    price[i].innerText = +(price[i].innerHTML) * 0.8;
+  }
   let discountSum = sumPrice * 0.8;
   cartSum.innerHTML = discountSum;
-  return cartSum
+ return cartSum
 }
 
 
